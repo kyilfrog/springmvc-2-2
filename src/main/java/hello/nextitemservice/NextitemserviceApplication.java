cb0ab49b18ebc85a20b9260622c0ab4d342ffee6
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-04-03 19:40:23 +0900

    입력 폼 처리

---
+++ src/main/java/hello/nextitemservice/NextitemserviceApplication.java
@@ -0,0 +1,13 @@
+package hello.nextitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextitemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextitemserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/nextitemservice/NextitemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.nextitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextitemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
